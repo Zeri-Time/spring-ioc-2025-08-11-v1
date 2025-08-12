4ab27e4657db9b41484025c13637137f6a8a5e5d
Zeri-Time <blee.940604@gmail.com>
2025-08-12 17:53:55 +0900

    feature/v1-first-draft

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,28 @@
 package com.ll.framework.ioc;
 
-public class ApplicationContext {
-    public ApplicationContext() {
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
+import com.ll.domain.testPost.testPost.service.TestPostService;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApplicationContext {
+    private final Map<String, Object> bean;
+
+    TestPostRepository testPostRepository = new TestPostRepository();
+    TestPostService testPostService = new TestPostService(testPostRepository);
+    TestFacadePostService testFacadePostService = new TestFacadePostService(testPostService, testPostRepository);
+
+    public ApplicationContext() {
+        this.bean = new HashMap<>();
+
+        bean.put("testPostRepository", testPostRepository);
+        bean.put("testPostService", testPostService);
+        bean.put("testFacadePostService", testFacadePostService);
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        return (T) bean.get(beanName);
     }
 }
