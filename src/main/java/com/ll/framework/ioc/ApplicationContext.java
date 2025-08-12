3545862b096b890bc6c86eef0ab9e48942af22b3
SWWWin <bgpd1258@naver.com>
2025-08-12 16:38:03 +0900

    feat: t3 해결

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -5,12 +5,19 @@ import com.ll.domain.testPost.testPost.service.TestPostService;
 
 public class ApplicationContext{
 
+    private static TestPostRepository testPostRepository;
+    private static TestPostService testPostService;
+
+    static {
+        testPostRepository = new TestPostRepository();
+        testPostService = new TestPostService(testPostRepository);
+    }
 
     public ApplicationContext() {
 
     }
 
     public TestPostService genBean(String beanName) {
-        return new TestPostService(new TestPostRepository());
+        return testPostService;
     }
 }
